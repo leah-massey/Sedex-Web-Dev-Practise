f498d932d3f360c23737a5866f623ed455a94599
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-03 17:50:57 +0000

    Feat: path variable for language introduced.

--- src/main/kotlin/com/example/Hello.kt
+++ src/main/kotlin/com/example/Hello.kt
@@ -9,17 +9,31 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequest
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 val app: HttpHandler = routes(
-    "/hello" bind GET to { req: Request ->
+    "{lang:[a-zA-Z-]+}/hello" bind GET to { req: Request ->
+
+//{lang:[a-zA-Z-]}
+        val lang = req.path("lang") ?: "en-US"
         val name = req.query("name") ?: ""
 
+        val greeting = when (lang) {
+            "en-US" -> "Hello"
+            "fr-FR" -> "Bonjour"
+            "en-AU" -> "G'day"
+            "it-IT" -> "Ciao"
+            "en-GB" -> "Good day"
+            else -> "Hello"
+        }
+
+
         if (name.matches(Regex("[a-zA-Z]+")) || name == "") {
-            // Name is a string of characters (alphabetic)
-            Response(OK).body("Hello $name")
+            // Name is a string of characters (alphabetic) or not declared
+            Response(OK).body("$greeting $name")
         } else {
             Response(BAD_REQUEST).body("Invalid name")
         }
