1aecca0a57a22d6c501e39d3194e24314c20bd01
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-05 18:08:57 +0000

    echo_headers endpoint updated so that if the client supports JSON responses, response body is JSON format with headers as key-value pairs.

--- src/main/kotlin/com/example/Hello.kt
+++ src/main/kotlin/com/example/Hello.kt
@@ -1,15 +1,23 @@
 package com.example
 
+import com.fasterxml.jackson.databind.JsonNode
+import netscape.javascript.JSObject
 import org.http4k.core.*
 import org.http4k.core.Method.GET
 import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.OK
 import org.http4k.filter.DebuggingFilters.PrintRequest
+import org.http4k.format.Jackson.asJsonArray
+import org.http4k.format.Jackson.asJsonObject
+import org.http4k.format.Jackson.asJsonValue
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
+import org.http4k.format.Jackson.auto
+
+
 
 val app: HttpHandler = routes(
     "{lang:[a-zA-Z-]+}/hello" bind GET to { req: Request ->
@@ -38,9 +46,27 @@ val app: HttpHandler = routes(
     ,
 
     "/echo_headers" bind GET to {req: Request ->
+
         val headers = req.headers
+
         val headersAsList = headers.map{"${it.first}: ${it.second}"}.joinToString("\n")
-        Response(OK).body("$headersAsList")
+        val headersAsJson: JsonNode = headers.toMap().asJsonObject()
+
+        val anyMediaType  = "*/*"
+        val jsonType = "json"
+
+        val value = headers.find { it.first == "Accept" }?.second
+
+        if (value != null && jsonType in value) {
+            println("Value supports json responses")
+            Response(OK).body("this is JSON format: $headersAsJson")
+        } else if (value != null && anyMediaType in value) {
+            println("Value supports all media type responses")
+            Response(OK).body("this is JSON format: $headersAsJson")
+        } else {
+            println("Value does not support json responses")
+            Response(OK).body(" this is list format: $headersAsList")
+        }
     }
 )
 fun main() {
