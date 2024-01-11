c9d2d4188863273837954e1e8e5809f6dc34e6ac
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-11 18:51:09 +0000

    Feat: .echoHeadersForJson method returning Map of key value pairs as expected

--- src/main/kotlin/com/example/HelloClient.kt
+++ src/main/kotlin/com/example/HelloClient.kt
@@ -3,25 +3,41 @@ package com.example
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.*
 import org.http4k.core.Method.GET
-import org.http4k.filter.DebuggingFilters.PrintResponse
 
 class HelloClient(private val baseURL: String) {
     private val client: HttpHandler = JavaHttpClient() // establish a httpHandler for the client side
-        fun sayHello(name: String? = null, language: String = "en-US"): String  = client(Request(GET, "${baseURL}/hello")
+    fun sayHello(name: String? = null, language: String = "en-US"): String =
+        client(Request(GET, "${baseURL}/hello")
             .query("name", name)
             .header("Accept-language", language)
-        ).bodyString()
+            ).bodyString()
 
-//        fun echo_headers(prefix: String? = null, headers: List<Pair<String, String?>>?): Response = client(Request(GET, "${baseURL}/echo_headers")
-//        .query("as_response_headers_with_prefix", prefix))
+    fun echoHeadersForJson(data: Map<String, String> ): Map<String, String> {
+
+        val dataAsList: List<Pair<String, String?>> = data.toList()
+        // construct req with accept header set to application json
+        // add all the pairs from data as request headers
+        val request = Request(GET, "${baseURL}/echo_headers").header("Accept", "application/json").headers(dataAsList)
+
+        // make request
+        val response: Response = client(request) // make the request with accept header set to application json
+        println(response.bodyString())
+
+        // convert response back to map and return it
+        val regex: Regex = Regex("\"(.*?)\":\"(.*?)\"")
+        val matches = regex.findAll(response.bodyString())
+        val responseBodyAsMap: Map<String, String> = matches.associate { it.groupValues[1] to it.groupValues[2] }
+        return responseBodyAsMap
+    }
+
+    // 3 functions each
+
 }
 
 fun main() {
     val baseUrl = "http://localhost:9000"
     val helloClient = HelloClient(baseUrl)
 
-    // Test - Call sayHello and print the response
-    val helloResponse = helloClient.sayHello(name = "Cyril", language = "fr-FR")
-    println("sayHello Response:")
+    println(helloClient.echoHeadersForJson(mapOf("testkey" to "testvalue")))
 
 }
