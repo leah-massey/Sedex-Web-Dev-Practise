dc4962f5dfcd98fc9af73f558c734e8569768a34
leah-massey <15648177+leah-massey@users.noreply.github.com>
2024-01-12 09:46:24 +0000

    Feat: .echoHeadersForNoJson method returning Map of key value pairs as expected

--- src/main/kotlin/com/example/HelloClient.kt
+++ src/main/kotlin/com/example/HelloClient.kt
@@ -20,13 +20,36 @@ class HelloClient(private val baseURL: String) {
         val request = Request(GET, "${baseURL}/echo_headers").header("Accept", "application/json").headers(dataAsList)
 
         // make request
-        val response: Response = client(request) // make the request with accept header set to application json
+        val response: Response = client(request)
+        println("this is response body")
         println(response.bodyString())
 
         // convert response back to map and return it
         val regex: Regex = Regex("\"(.*?)\":\"(.*?)\"")
         val matches = regex.findAll(response.bodyString())
         val responseBodyAsMap: Map<String, String> = matches.associate { it.groupValues[1] to it.groupValues[2] }
+        return responseBodyAsMap
+    }
+
+    fun echoHeadersForNoJson(data: Map<String, String>): Map<String, String> {
+
+
+        val dataAsList: List<Pair<String, String>> = data.toList()
+
+        // construct req with header not set to application json
+        // add all the pairs from data as request headers
+        var request: Request = Request(GET, "${baseURL}/echo_headers").header("Accept", "text/html").headers(dataAsList)
+
+        // make request
+        val responseBody: String = client(request).bodyString()
+
+        //convert response back to map and return it
+        val responseBodyAsMap: Map<String, String> = responseBody.split("\n")
+            .associate { line ->
+                val parts = line.split(": ")
+                parts[0] to parts[1]
+            }
+
         return responseBodyAsMap
     }
 
@@ -38,6 +61,10 @@ fun main() {
     val baseUrl = "http://localhost:9000"
     val helloClient = HelloClient(baseUrl)
 
-    println(helloClient.echoHeadersForJson(mapOf("testkey" to "testvalue")))
+//    println("First is res.body, second is actual")
+//    println(helloClient.echoHeadersForJson(mapOf("testkey" to "testvalue")))
+
+    println("First is res.body, second is actual")
+    println(helloClient.echoHeadersForNoJson(mapOf("testkey" to "testvalue")))
 
 }
